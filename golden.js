2390fd26fe79fe6368202b6af5a2adfba01f2e43
saanvi272 <73384497+saanvi272@users.noreply.github.com>
2021-01-31 15:20:44 +0530

    Add files via upload

---
+++ golden.js
@@ -0,0 +1,25 @@
+class Golden{
+    constructor(x,y,width,height){
+        var options={
+            friction:0,
+            density:0.001
+        }
+        this.body=Bodies.rectangle(x,y,width,height,options);
+        this.width=width;
+        this.height=height;
+        this.image=loadImage("golden.png");
+        World.add(world,this.body);
+
+    }
+
+    display(){
+        push();
+        translate(this.body.position.x,this.body.position.y);
+        rotate(this.body.angle);
+        imageMode(CENTER);
+        image(this.image,0,0,this.width,this.height);
+        pop();
+
+
+    }
+}
